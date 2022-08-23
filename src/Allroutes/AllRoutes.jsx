f6861a644e7799c93f02035f7f4038b753e1d7e9
jstgrowup <deysubham999@gmail.com>
2022-08-23 18:02:16 +0530

    added

---
+++ src/Allroutes/AllRoutes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Home from './Home'
+export default function AllRoutes() {
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route ></Route>
+                <Route></Route>
+                <Route></Route>
+
+            </Routes>
+        </>
+    )
+}
