3d48467026edc051128dd2b83ddec9ccad12907d
jstgrowup <deysubham999@gmail.com>
2022-08-26 22:10:53 +0530

    added

--- src/SparkCities/CommonNavbar.tsx
+++ src/SparkCities/CommonNavbar.tsx
@@ -68,41 +68,7 @@ export default function CommonNavbar() {
             ></Image>
           </Box>
 
-          {/* textAlign={useBreakpointValue({ base: "center", md: "left" })}
-            fontFamily={"heading"}
-            color={useColorModeValue("gray.800", "white")}
-          >
-           
-          </Text> */}
-          {/* <Stack
-          flex={{ base: 1, md: 0 }}
-          justify={"flex-end"}
-          direction={"row"}
-          spacing={6}
-        >
-          <Button
-            as={"a"}
-            fontSize={"sm"}
-            fontWeight={400}
-            variant={"link"}
-            href={"#"}
-          >
-            Sign In
-          </Button>
-          <Button
-            display={{ base: "none", md: "inline-flex" }}
-            fontSize={"sm"}
-            fontWeight={600}
-            color={"white"}
-            bg={"pink.400"}
-            // href={'#'}
-            _hover={{
-              bg: "pink.300",
-            }}
-          >
-            Sign Up
-          </Button>
-           </Stack> */}
+        
           <Flex display={{ base: "none", md: "flex" }} ml={10}>
             <DesktopNav />
           </Flex>
